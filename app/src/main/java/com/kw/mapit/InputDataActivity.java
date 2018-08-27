1bc02b71fce0201ffb55ec9fde9fe0f96a45056e
Sunha03 <ahnus123@gmail.com>
2018-08-27 14:45:01 +0900

    Customize Font(SeoulNamsan)

---
+++ app/src/main/java/com/kw/mapit/InputDataActivity.java
@@ -0,0 +1,175 @@
+package com.kw.mapit;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class InputDataActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "APISigninView";
+    StringBuffer sbParams = null;
+
+    TextView tv_signIn;
+    EditText loginId;
+    EditText loginPassword;
+    EditText loginEmail;
+    EditText loginSex;
+    EditText loginAge;
+
+    String userName;
+    String userPassword;
+    String userSex;
+    int userAge;
+    String userEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_data);
+
+        Intent getIntent = new Intent();
+        userName = getIntent.getStringExtra("USERNAME");
+        userEmail = getIntent.getStringExtra("USEREMAIL");
+
+        loginId = (EditText)findViewById(R.id.ed_api_signin_id);
+        loginPassword = (EditText)findViewById(R.id.ed_api_signin_password);
+        loginSex = (EditText)findViewById(R.id.ed_api_signin_sex);
+        loginAge = (EditText)findViewById(R.id.ed_api_signin_age);
+        loginEmail = (EditText)findViewById(R.id.ed_api_signin_email);
+
+        loginId.setText(userName);
+        loginEmail.setText(userEmail);
+
+        //tv_signIn = (TextView)findViewById(R.id.tv_signIn);
+        //tv_signIn.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/08seoulnamsanm.ttf"));
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_sign_in)
+        {
+            //EditText에서 정보 받아오기
+            userName = loginId.getText().toString();
+            userPassword = loginPassword.getText().toString();
+            userSex = loginSex.getText().toString();
+            try{
+                userAge = Integer.parseInt(loginAge.getText().toString());
+            }
+            catch (NumberFormatException e){
+                Log.e("superdroid","UserAge is Not Integer");
+            }
+            userEmail = loginEmail.getText().toString();
+
+            Log.e("superdroid", userName + " - " + userPassword+ " - " + userSex+ " - " + userAge+ " - " + userEmail);
+
+            //Insert 사용자 정보
+            sbParams = new StringBuffer();
+            sbParams.append("id=").append(userName);
+            sbParams.append("&").append("password=").append(userPassword);
+            sbParams.append("&").append("sex=").append(userSex);
+            sbParams.append("&").append("age=").append(userAge);
+            sbParams.append("&").append("email=").append(userEmail);
+
+            InsertData task = new InsertData();
+            task.execute(userName);
+
+            loginId.setText("");
+            loginPassword.setText("");
+            loginSex.setText("");
+            loginAge.setText("");
+            loginEmail.setText("");
+
+            //DbConnectActivity로 이동
+            redirectDbConnectActivity();
+        }
+    }
+
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(InputDataActivity.this,"please wait",null,true, true);
+        }
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(LOG_TAG,"POST response - " + result);
+        }
+        @Override
+        protected String doInBackground(String... params) {
+
+            String hashtag = (String)params[0];
+
+            String serverURL = "http://192.168.0.4/insertUserData.php";
+
+            try{
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(sbParams.toString().getBytes("UTF-8"));
+                //outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode=httpURLConnection.getResponseCode();
+                Log.d(LOG_TAG,"POST respose code - " +responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString();
+
+            } catch (Exception e) {
+                Log.e(LOG_TAG,"InsertData: Error ",e);
+
+                return new String("Error: "+e.getMessage());
+            }
+        }
+    }
+
+    //MainActivity로 이동
+    private void redirectDbConnectActivity() {
+        Intent userProfileIntent = new Intent(this, DbConnectActivity.class);
+        startActivity(userProfileIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/kw/mapit/LoginActivity.java
+++ app/src/main/java/com/kw/mapit/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.kw.mapit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,5 +19,12 @@ public class LoginActivity extends AppCompatActivity {
         EditText edit_pw= (EditText)findViewById(R.id.ed_login_password);
         edit_id.setTextColor(Color.BLACK);
         edit_pw.setTextColor(Color.BLACK);
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_sign_in) {
+            Intent goIntent = new Intent(this, InputDataActivity.class);
+            startActivity(goIntent);
+        }
     }
 }
